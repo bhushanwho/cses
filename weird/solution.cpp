19009050c888d436e2fa48429d459f33acda8f8f
bhushan <bhushanwho@gmail.com>
2025-06-19 12:55:49 +0530

    1068

---
+++ weird/solution.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h> 
+using namespace std; 
+ 
+#define ll long long int 
+#define vi vector<int> 
+#define vvi vector<vector<int>> 
+#define vll vector<long long int> 
+// #define ONLINE_JUDGE 1 
+
+// https://cses.fi/problemset/task/1068/
+ 
+int main() { 
+    #ifndef ONLINE_JUDGE 
+    freopen("in.txt", "r", stdin); 
+    freopen("out.txt", "w", stdout); 
+    #endif 
+    std::ios::sync_with_stdio(false); 
+    
+    ll n;
+    cin >> n;
+
+    unordered_set<ll> us;
+
+    while( us.find(n) == us.end() ) {
+        us.insert(n);
+        cout << n << " ";
+        // isn't 1 an odd number through? idk, weird test cases
+        if( n == 1) break;
+        n = n%2 ? 3*n+1 : n/2;
+    }
+
+    cout << '\n';
+ 
+    return 0; 
+} 
