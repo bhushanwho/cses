7d4ca8827f4e90fac3d3905e737f6b5d7629206e
bhushan <bhushanwho@gmail.com>
2025-06-22 12:43:51 +0530

    1084

---
+++ ferriswheel/solution.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h> 
+using namespace std; 
+ 
+#define ll long long int 
+#define vi vector<int> 
+#define vvi vector<vector<int>> 
+#define vll vector<long long int> 
+// #define ONLINE_JUDGE 1 
+
+// https://cses.fi/problemset/task/1090
+ 
+int main() { 
+    #ifndef ONLINE_JUDGE 
+    freopen("in.txt", "r", stdin); 
+    freopen("out.txt", "w", stdout); 
+    #endif 
+    std::ios::sync_with_stdio(false); 
+ 
+    int n, x;
+    cin >> n >> x;
+
+    // TODO
+
+    return 0; 
+} 
---
+++ missing/solution.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h> 
+using namespace std; 
+ 
+#define ll long long int 
+#define vi vector<int> 
+#define vvi vector<vector<int>> 
+#define vll vector<long long int> 
+// #define ONLINE_JUDGE 1 
+ 
+int main() { 
+    #ifndef ONLINE_JUDGE 
+    freopen("in.txt", "r", stdin); 
+    freopen("out.txt", "w", stdout); 
+    #endif 
+    std::ios::sync_with_stdio(false); 
+ 
+ 
+    return 0; 
+} 
